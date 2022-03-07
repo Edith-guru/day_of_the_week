ab2492ca6ba93eb295946fecd847ae9e814d3be2
Edith-guru <edithnesa@gmail.com>
2022-03-07 14:39:00 +0300

    Added template and deleted test style.css

---
+++ script.js
@@ -0,0 +1,20 @@
+const male = {
+    Sunday: Kwasi, 
+    Monday: Kwadwo, 
+    Tuesday: Kwabena, 
+    Wednesday: Kwaku, 
+    Thursday:  Yaw, 
+    Friday: Kofi, 
+    Saturday: Kwame
+};
+
+const female = {
+    Sunday: Akosua,
+    Monday: Adwoa,
+    Tuesday: Abenaa,
+    Wednesday: Akua,
+    Thursday:  Yaa,
+    Friday: Afua,
+    Saturday: Ama,
+};
+
